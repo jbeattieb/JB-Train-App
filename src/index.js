37baa219a3530325b952e73901ffa6cde28d2b52
jbeattieb <beattielol7@gmail.com>
2025-06-02 15:40:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TrainAlertApp from './TrainAlertApp';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<TrainAlertApp />);
 
